77143c37bfa8f62bb29533ce80e78016fcafc697
ThomasM <thomas.maugeais@epfedu.fr>
2025-02-12 14:55:42 +0100

    petit fils de greg

--- src/main/java/org/oxyl/Triangle.java
+++ src/main/java/org/oxyl/Triangle.java
@@ -36,4 +36,4 @@ public class Triangle {
         y3 += distanceY;
     }
 }
-// Ouais cest Greg. Oui c'est le fils à Greg. Oui c'est
+// Ouais cest Greg. Oui c'est le fils à Greg. Oui c'est le petit fils de greg
